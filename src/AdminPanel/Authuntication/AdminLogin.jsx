6017a904d77090f38256585f45755844be1b565c
Hiren Ray <166147435+GoingOnLoardShiva@users.noreply.github.com>
2025-05-23 00:53:10 +0530

    git second comit

--- src/AdminPanel/Authuntication/AdminLogin.jsx
+++ src/AdminPanel/Authuntication/AdminLogin.jsx
@@ -4,8 +4,8 @@ const AdminLogin = () => {
   const handleGitHubLogin = () => {
     account.createOAuth2Session(
       "github",
-      "https://codetech-tau.vercel.app/Ssucess", // where to go after success
-      "https://localhost:3000/Ssucess" // where to go after failure
+      "https://codetech-tau.vercel.app/AdminLogin",
+      "https://codetech-tau.vercel.app/"
     );
   };
   return (
