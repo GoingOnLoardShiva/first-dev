fd6065b0b9b7e4aa1a8ea7656def759e559c0a00
Hiren Ray <166147435+GoingOnLoardShiva@users.noreply.github.com>
2025-05-23 00:57:05 +0530

    third git

--- src/AdminPanel/Authuntication/Appwrite.js
+++ src/AdminPanel/Authuntication/Appwrite.js
@@ -1,8 +1,9 @@
 import { Client, Account } from "appwrite";
 
-const client = new Client()
-    .setEndpoint("https://cloud.appwrite.io/v1")
-    .setProject("682f6b32002e9e6c1dcd");
+const client = new Client();
+client
+  .setEndpoint("https://fra.cloud.appwrite.io/v1")
+  .setProject("682f6b32002e9e6c1dcd");
 
 const account = new Account(client);
 
